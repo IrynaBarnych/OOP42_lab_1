379df44400fd35222296df7c489ca4a2d1bb9086
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-13 23:38:38 +0200

    Завдання 2 Показати статистику студентів. Має відображатися назва країни та кількість студентів з цієї країни.

--- lab1.sql
+++ lab1.sql
@@ -1,9 +1,9 @@
 --Завдання 2
---Показати статистику міст. Має відображатися назва
---міста та кількість студентів з цього міста..
+--Показати статистику студентів. Має відображатися
+--назва країни та кількість студентів з цієї країни.
 
-SELECT city, COUNT (*) AS student_count
+SELECT country, COUNT (*) AS student_count
 FROM student_grades
-GROUP BY city
+GROUP BY country
 
 
