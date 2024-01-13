0ce83f991b464cf6fe775a12fea6ad9f22385485
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-13 23:10:40 +0200

    Завдання 1 Показати інформацію про студентів з віком, у вказаному діапазоні.

--- lab1.sql
+++ lab1.sql
@@ -1,14 +1,8 @@
 --Завдання 1
---Показати інформацію про студентів, 
---яким виповнилося 20 років.
+--Показати інформацію про студентів з віком, 
+--у вказаному діапазоні.
 
-SELECT *
+SELECT * 
 FROM student_grades
-WHERE date_of_birth <= current_date - interval '25 years';
---отримати вік студентів
-SELECT
-  student_name,
-  date_of_birth,
-  EXTRACT(YEAR FROM AGE(current_date, date_of_birth)) AS age
-FROM
-  student_grades;
+WHERE EXTRACT(YEAR FROM AGE(current_date, date_of_birth)) 
+BETWEEN 18 AND 24;
